4f98bb717410ebacc1c2026319dbac28876b574a
Asta Havshoj <astah2000@gmail.com>
2025-01-30 17:38:28 +0100

    dynamiskSingle færdig

---
+++ product.js
@@ -0,0 +1,28 @@
+let product_container = document.querySelector(".product_container");
+let productId = 1163;
+
+fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+  .then((response) => response.json())
+  .then((data) => {
+    product_container.innerHTML = `
+        <div class="product-image">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="Sahara Team India Jersey" />
+      </div>
+      <div class="product-info">
+        <h2>${data.productdisplayname}</h2>
+        <p><strong>Mærke:</strong> ${data.brandname}</p>
+        <p><strong>Farve:</strong> ${data.basecolour} </p>
+        <p><strong>Varenummer:</strong> ${data.id} </p></p>
+        <p class="price">${data.price},-</p>
+        <label for="size">Vælg en størrelse:</label>
+        <select id="size">
+          <option>S</option>
+          <option>M</option>
+          <option>L</option>
+          <option>XL</option>
+        </select>
+        <button id="add-to-basket" disabled>Læg i kurv</button>
+      </div>
+    
+    `;
+  });
